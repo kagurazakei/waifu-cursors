229c80fc2760715b5fa60c03402571b870ed8ed1
kagurazakei <maotsugiri@gmail.com>
2025-07-15 13:29:15 +0630

    ✅ test(flake): test flake update

--- flake.nix
+++ flake.nix
@@ -1,25 +1,29 @@
+### my collection of cursors
 {
   description = "Waifu cursor themes flake";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: 
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
 
-      cursorSrc = self;
+    cursorSrc = self;
 
-      themeNames = [
-        "Hoshimi-Miyami"
-        "Imouto"
-        "Kirispica"
-        "Menace-Mamaki"
-        "miku-cursor-linux"
-        "Reichi-Shinigami"
-      ];
+    themeNames = [
+      "Hoshimi-Miyami"
+      "Imouto"
+      "Kirispica"
+      "Menace-Mamaki"
+      "miku-cursor-linux"
+      "Reichi-Shinigami"
+    ];
 
-      mkCursor = name: pkgs.stdenv.mkDerivation {
+    mkCursor = name:
+      pkgs.stdenv.mkDerivation {
         pname = "waifu-cursor-${name}";
         version = "1.0";
 
@@ -38,37 +42,37 @@
         };
       };
 
-      allThemes = pkgs.stdenv.mkDerivation {
-        pname = "waifu-cursors-all";
-        version = "1.0";
-        src = cursorSrc;
+    allThemes = pkgs.stdenv.mkDerivation {
+      pname = "waifu-cursors-all";
+      version = "1.0";
+      src = cursorSrc;
 
-        installPhase = ''
-          mkdir -p $out/share/icons
-          ${pkgs.lib.concatMapStringsSep "\n" (name: ''
+      installPhase = ''
+        mkdir -p $out/share/icons
+        ${pkgs.lib.concatMapStringsSep "\n" (name: ''
             cp -r $src/${name} $out/share/icons/${name}
-          '') themeNames}
-        '';
+          '')
+          themeNames}
+      '';
 
-        meta = with pkgs.lib; {
-          description = "All waifu cursor themes bundled together";
-          license = licenses.mit;
-          platforms = platforms.linux;
-          homepage = "https://github.com/maotseantonio/waifu-cursors";
-        };
+      meta = with pkgs.lib; {
+        description = "All waifu cursor themes bundled together";
+        license = licenses.mit;
+        platforms = platforms.linux;
+        homepage = "https://github.com/maotseantonio/waifu-cursors";
       };
-
-    in
-    {
-      packages.${system} =
-        (builtins.listToAttrs (map (name: {
+    };
+  in {
+    packages.${system} =
+      (builtins.listToAttrs (map (name: {
           inherit name;
           value = mkCursor name;
-        }) themeNames)) // {
-          all = allThemes;
-        };
+        })
+        themeNames))
+      // {
+        all = allThemes;
+      };
 
-      defaultPackage.${system} = allThemes;
-    };
+    defaultPackage.${system} = allThemes;
+  };
 }
-
